e13489495a82bb33075319f5470722d130deddeb
Jordan Koschei <jordan@askspoke.com>
2019-08-03 18:58:50 -0400

    Again

--- single.php
+++ single.php
@@ -35,6 +35,9 @@
 
       <div class="post-content">
         <?php the_content(); ?>
+        <?php if (get_field('edited')) : ?>
+          <span class="post-edited">This interview has been edited for length and clarity.</span>
+        <? endif; ?>
         <span class="post-date icon icon-date">Published on <?php the_date(); ?></span>
       </div>
     </div>
