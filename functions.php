22992d6d697004a00a0b4f38067231c4031f6adb
Jordan Koschei <jordan@askspoke.com>
2019-08-03 22:04:22 -0400

    Disable comments on galleries

--- functions.php
+++ functions.php
@@ -44,3 +44,12 @@ function register_menus() {
   add_theme_support( 'post-thumbnails' );
   add_theme_support( 'responsive-embeds' );
 }
+
+function tweakjp_rm_comments_att( $open, $post_id ) {
+    $post = get_post( $post_id );
+    if( $post->post_type == 'attachment' ) {
+        return false;
+    }
+    return $open;
+}
+add_filter( 'comments_open', 'tweakjp_rm_comments_att', 10 , 2 );
