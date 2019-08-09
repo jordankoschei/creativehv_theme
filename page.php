f09db6c7e16c7c9a352be8eb8fddaae5c598f9bc
Jordan Koschei <jordan@askspoke.com>
2019-08-08 21:15:39 -0400

    Fix card spacing

--- page.php
+++ page.php
@@ -36,10 +36,6 @@
         <a href="<?php echo site_url(); ?>/suggest-an-interview/" class="post-card--blank__link">Suggest an interview</a>
       </div>
     </div>
-
-    <?php if ($query->found_posts % 2 != 0) : ?>
-      <div class="post-card post-card--empty"></div>
-    <?php endif; ?>
   <?php endif; ?>
 </div>
 
