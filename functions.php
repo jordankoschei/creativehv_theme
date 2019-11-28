357a56a5abdd735cadfe2cc97036086d52656bcd
Jordan Koschei <jordan@askspoke.com>
2019-11-28 09:13:04 -0500

    Shrink the menu

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 function add_theme_scripts() {
-  $cache_buster = 1574915752143;
+  $cache_buster = 1574950343994;
   wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/app.min.css', false, $cache_buster, 'all');
 }
 add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
