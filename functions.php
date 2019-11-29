eb5093cd997b1a72a898a5897ede397e1057e6ef
Jordan Koschei <jordan@askspoke.com>
2019-11-29 11:18:42 -0500

    Removing cruft and organizing functions.php

--- functions.php
+++ functions.php
@@ -1,161 +1,199 @@
 <?php
-function add_theme_scripts() {
-  $cache_buster = 1574955090542;
-  wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/app.min.css', false, $cache_buster, 'all');
-}
-add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
-// Add description to RSS
-function update_rss($content) {
-  if ( is_feed() ){
-    $content = '<p>' . get_field('description') . '</p>' . $content;
+// ----------------------------------------------------------------------------------------------//
+// ----------------------------------------------------------------------------------------------//
+// REGISTER IMPORTANT THINGS --------------------------------------------------------------------//
+// ----------------------------------------------------------------------------------------------//
+// ----------------------------------------------------------------------------------------------//
+
+// Add CSS (and JS, eventually, maybe) to <head>
+// Also includes the cache-busting via gulpfile
+  function add_theme_scripts() {
+    $cache_buster = 1574955090542;
+    wp_enqueue_style( 'style', get_template_directory_uri() . '/assets/css/app.min.css', false, $cache_buster, 'all');
   }
-  return $content;
-}
-add_filter('the_content', 'update_rss');
+  add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
-function update_rss_excerpt($content) {
-  return get_field('description');
-}
-add_filter('the_excerpt_rss', 'update_rss_excerpt');
+// Register counties & locations
+  function cptui_register_my_taxes() {
+    $labels = [
+      "name" => __( "Locations", "custom-post-type-ui" ),
+      "singular_name" => __( "Location", "custom-post-type-ui" ),
+    ];
 
+    $args = [
+      "label" => __( "Locations", "custom-post-type-ui" ),
+      "labels" => $labels,
+      "public" => true,
+      "publicly_queryable" => true,
+      "hierarchical" => true,
+      "show_ui" => true,
+      "show_in_menu" => true,
+      "show_in_nav_menus" => true,
+      "query_var" => true,
+      "rewrite" => [ 'slug' => 'location', 'with_front' => true, ],
+      "show_admin_column" => false,
+      "show_in_rest" => true,
+      "rest_base" => "location",
+      "rest_controller_class" => "WP_REST_Terms_Controller",
+      "show_in_quick_edit" => false,
+      ];
+    register_taxonomy( "location", [ "post" ], $args );
 
+    $labels = [
+      "name" => __( "Counties", "custom-post-type-ui" ),
+      "singular_name" => __( "County", "custom-post-type-ui" ),
+    ];
 
-// Hide password-protected posts
-function wpb_password_post_filter( $where = '' ) {
+    $args = [
+      "label" => __( "Counties", "custom-post-type-ui" ),
+      "labels" => $labels,
+      "public" => true,
+      "publicly_queryable" => true,
+      "hierarchical" => true,
+      "show_ui" => true,
+      "show_in_menu" => true,
+      "show_in_nav_menus" => true,
+      "query_var" => true,
+      "rewrite" => [ 'slug' => 'county', 'with_front' => true, ],
+      "show_admin_column" => false,
+      "show_in_rest" => true,
+      "rest_base" => "county",
+      "rest_controller_class" => "WP_REST_Terms_Controller",
+      "show_in_quick_edit" => false,
+      ];
+    register_taxonomy( "county", [ "post" ], $args );
+  }
+  add_action( 'init', 'cptui_register_my_taxes' );
+
+// Register menus
+  add_action( 'after_setup_theme', 'register_menus' );
+  function register_menus() {
+    register_nav_menu( 'header', 'Header Menu' );
+    register_nav_menu( 'footer', 'Footer Menu' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'responsive-embeds' );
+  }
+
+// The RSS version of each post didn't include the big intro paragraph (the "description")
+// so we add it manually here
+  function update_rss($content) {
+    if ( is_feed() ){
+      $content = '<p>' . get_field('description') . '</p>' . $content;
+    }
+    return $content;
+  }
+  add_filter('the_content', 'update_rss');
+
+// Override the excerpt with the post description
+  function update_rss_excerpt($content) {
+    return get_field('description');
+  }
+  add_filter('the_excerpt_rss', 'update_rss_excerpt');
+
+// Hide password-protected posts from the loop
+  function wpb_password_post_filter( $where = '' ) {
     if (!is_single() && !is_admin()) {
-        $where .= " AND post_password = ''";
+      $where .= " AND post_password = ''";
     }
     return $where;
-}
-add_filter( 'posts_where', 'wpb_password_post_filter' );
+  }
+  add_filter( 'posts_where', 'wpb_password_post_filter' );
+
+
+// ----------------------------------------------------------------------------------------------//
+// ----------------------------------------------------------------------------------------------//
+// QUALITY OF LIFE ------------------------------------------------------------------------------//
+// ----------------------------------------------------------------------------------------------//
+// ----------------------------------------------------------------------------------------------//
+
+// Remove junk from head
+  remove_action( 'wp_head', 'rsd_link' );
+  remove_action( 'wp_head', 'wp_generator' );
+  remove_action( 'wp_head', 'feed_links', 2 );
+  remove_action( 'wp_head', 'index_rel_link' );
+  remove_action( 'wp_head', 'wlwmanifest_link' );
+  remove_action( 'wp_head', 'feed_links_extra', 3 );
+  remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+  remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+  remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
+  remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+  remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
 
 // Disable emoji replacement
-function disable_emojis() {
-  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-  remove_action( 'wp_print_styles', 'print_emoji_styles' );
-  remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
-  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
-  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-  add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
-  add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
-}
-add_action( 'init', 'disable_emojis' );
-
-function disable_emojis_tinymce( $plugins ) {
-  if ( is_array( $plugins ) ) {
-    return array_diff( $plugins, array( 'wpemoji' ) );
-  } else {
-    return array();
+  function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    add_filter( 'wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2 );
   }
-}
+  add_action( 'init', 'disable_emojis' );
 
-function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
-  if ( 'dns-prefetch' == $relation_type ) {
-    $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
-    $urls = array_diff( $urls, array( $emoji_svg_url ) );
+  function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+      return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+      return array();
+    }
   }
 
-  return $urls;
-}
+  function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+    if ( 'dns-prefetch' == $relation_type ) {
+      $emoji_svg_url = apply_filters( 'emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/' );
+      $urls = array_diff( $urls, array( $emoji_svg_url ) );
+    }
 
-add_action( 'after_setup_theme', 'register_menus' );
-function register_menus() {
-  register_nav_menu( 'header', 'Header Menu' );
-  register_nav_menu( 'footer', 'Footer Menu' );
-  add_theme_support( 'post-thumbnails' );
-  add_theme_support( 'responsive-embeds' );
-}
+    return $urls;
+  }
 
-function tweakjp_rm_comments_att( $open, $post_id ) {
+// Remove comments from media attachments
+  function tweakjp_rm_comments_att( $open, $post_id ) {
     $post = get_post( $post_id );
     if( $post->post_type == 'attachment' ) {
         return false;
     }
     return $open;
-}
-add_filter( 'comments_open', 'tweakjp_rm_comments_att', 10 , 2 );
-
-
-// remove update notice for certain plugins
-add_filter( 'site_transient_update_plugins', function ($value) {
-  if ( isset( $value ) && is_object( $value ) ) {
-    unset( $value->response[ 'wp-migrate-db-pro/wp-migrate-db-pro.php' ] );
-    unset( $value->response[ 'wp-migrate-db-pro-media-files/wp-migrate-db-pro-media-files.php' ] );
   }
+  add_filter( 'comments_open', 'tweakjp_rm_comments_att', 10 , 2 );
 
-  return $value;
-});
-
-add_action('admin_head', function () {
-  ?>
-  <style>
-    .wpmdbpro-custom {
-      display: none;
+// Remove update notice for certain plugins
+  add_filter( 'site_transient_update_plugins', function ($value) {
+    if ( isset( $value ) && is_object( $value ) ) {
+      unset( $value->response[ 'wp-migrate-db-pro/wp-migrate-db-pro.php' ] );
+      unset( $value->response[ 'wp-migrate-db-pro-media-files/wp-migrate-db-pro-media-files.php' ] );
     }
-  </style>
-  <?php
-});
+
+    return $value;
+  });
+
+// Remove plugin cruft in admin
+  add_action('admin_head', function () {
+    ?>
+    <style>
+      .wpmdbpro-custom {
+        display: none;
+      }
+
+      .wpseo_content_cell#sidebar-container,
+      .yoast_premium_upsell,
+      #yoast-helpscout-beacon {
+        display: none;
+      }
+    </style>
+    <?php
+  });
 
 
-function cptui_register_my_taxes() {
+// Disable Yoast's Hidden love letter about using the WordPress SEO plugin.
+  add_action( 'template_redirect', function () {
+    if ( ! class_exists( 'WPSEO_Frontend' ) ) { return; }
+    $instance = WPSEO_Frontend::get_instance();
+    if ( ! method_exists( $instance, 'debug_mark') ) { return; }
 
-  /**
-   * Taxonomy: Locations.
-   */
-
-  $labels = [
-    "name" => __( "Locations", "custom-post-type-ui" ),
-    "singular_name" => __( "Location", "custom-post-type-ui" ),
-  ];
-
-  $args = [
-    "label" => __( "Locations", "custom-post-type-ui" ),
-    "labels" => $labels,
-    "public" => true,
-    "publicly_queryable" => true,
-    "hierarchical" => true,
-    "show_ui" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "query_var" => true,
-    "rewrite" => [ 'slug' => 'location', 'with_front' => true, ],
-    "show_admin_column" => false,
-    "show_in_rest" => true,
-    "rest_base" => "location",
-    "rest_controller_class" => "WP_REST_Terms_Controller",
-    "show_in_quick_edit" => false,
-    ];
-  register_taxonomy( "location", [ "post" ], $args );
-
-  /**
-   * Taxonomy: Counties.
-   */
-
-  $labels = [
-    "name" => __( "Counties", "custom-post-type-ui" ),
-    "singular_name" => __( "County", "custom-post-type-ui" ),
-  ];
-
-  $args = [
-    "label" => __( "Counties", "custom-post-type-ui" ),
-    "labels" => $labels,
-    "public" => true,
-    "publicly_queryable" => true,
-    "hierarchical" => true,
-    "show_ui" => true,
-    "show_in_menu" => true,
-    "show_in_nav_menus" => true,
-    "query_var" => true,
-    "rewrite" => [ 'slug' => 'county', 'with_front' => true, ],
-    "show_admin_column" => false,
-    "show_in_rest" => true,
-    "rest_base" => "county",
-    "rest_controller_class" => "WP_REST_Terms_Controller",
-    "show_in_quick_edit" => false,
-    ];
-  register_taxonomy( "county", [ "post" ], $args );
-}
-add_action( 'init', 'cptui_register_my_taxes' );
+    remove_action( 'wpseo_head', array( $instance, 'debug_mark' ), 2 );
+  }, 9999 );
--- header.php
+++ header.php
@@ -1,31 +1,26 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>" />
-  <title><?php wp_title(); ?></title>
-  <link rel="profile" href="http://gmpg.org/xfn/11" />
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<title><?php wp_title(); ?></title>
+<?php wp_head(); ?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="alternate" type="application/rss+xml" title="Creative Hudson Valley" href="<?php bloginfo('url'); ?>/?feed=rss2" />
 
-  <?php if (get_home_url() == 'http://creativehv.test') : ?>
-  <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/dev-favicon.png" sizes="16x16" />
-  <?php endif; ?>
+<?php if (get_home_url() == 'http://creativehv.test') : ?>
+<link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/assets/img/dev-favicon.png" sizes="16x16" />
+<?php endif; ?>
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-143769687-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  <link rel="alternate" type="application/rss+xml" title="Creative Hudson Valley" href="<?php bloginfo('url'); ?>/?feed=rss2" />
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-143769687-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-143769687-1');
-  </script>
-
+  gtag('config', 'UA-143769687-1');
+</script>
 </head>
+
 <body <?php body_class(); ?>>
 
 <header class="header">
